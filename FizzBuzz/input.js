8c49bf99bde2a53f4c1bd18d44cd7b24e4cd9a56
RogueGitting <digitalenterprise9@gmail.com>
2018-10-05 14:15:28 +1000

    Done for now

--- FizzBuzz/input.js
+++ FizzBuzz/input.js
@@ -1,13 +1,51 @@
+var to = 0;
+var e = setInterval(load, 50);
+var seconds = 0;
+var numbers = { 1:3, 2:5 };
+var words = { 1:'Fizz', 2:'Buzz' };
+
+Object.objsize = function(Myobj) {
+    objlen = Object.keys(Myobj).length;
+	return objlen;
+};
+
+function configure() {
+	
+}
+
+function second() {
+	seconds = seconds + 1;
+}
+
+function load() {
+	to = document.getElementById("goto").value;
+	console.log(to);
+}
+
+function startfizz() {
+	document.getElementById("generating").style.display = "block";
+	var d = setInterval(second, 100);
+	setTimeout(function() {
+		fizzbuzz();
+	}, 1);
+}
 
 function fizzbuzz() {
+	document.getElementById("generating").style.display = "none";
 	var mobileoutput = "";
-	for (var i = 1; i <= 100; i++) {
+	var ed = Object.objsize(numbers);
+	var es = Object.objsize(words);
+	for (var i = 1; i <= to; i++) {
 		var output = "";
-		if (i % 3 == 0) { output += "Fizz"; }
-		if (i % 5 == 0) { output += "Buzz"; }
+		for (var e = 1; e <= ed; e++) {
+			if (i % numbers[e] == 0) { output += words[e]; }
+		}
+		/*if (i % 3 == 0) { output += "Fizz"; }
+		if (i % 5 == 0) { output += "Buzz"; }*/
 		if (output == "") { output += i; }
 		console.log(output);
 		mobileoutput += output+"<br>";
 		document.getElementById("out").innerHTML = mobileoutput;
+		console.log(seconds/10);
 	}
 }

a83733541eb82439d633ac7a7f572137e474bfe7
Roguim <39052241+Roguim@users.noreply.github.com>
2020-03-16 21:26:04 +1100

    Header Links Demo

---
+++ JSUtils/Demos/HeaderLinks/HeaderLinks.js
@@ -0,0 +1,25 @@
+var headernumber = 0;
+function MakeHeader(element) {
+    if (element.classList.contains('invisible')) {
+        return;
+    } else {
+        if (element.id == "") {
+            headernumber += 1;
+            element.id = `${headernumber}`;
+        }
+        name = element.innerHTML;
+        link = document.createElement('a');
+        link.innerHTML = name;
+        link.classList.add(element.nodeName);
+        link.href = document.URL.slice(0, -1)+`#${element.id}`;
+        document.getElementsByTagName('header')[0].append(link);
+    }
+}
+for (n = 1; n <= 6; n++) {
+    for (i = 0; i < document.getElementsByTagName(`h${n}`).length; i++) {
+        document.getElementsByTagName(`h${n}`)[i].classList.add('HeaderLinks-Heading');
+    }
+}
+for (i = 0; i < document.getElementsByClassName('HeaderLinks-Heading').length; i++) {
+    MakeHeader(document.getElementsByClassName('HeaderLinks-Heading')[i]);
+}
